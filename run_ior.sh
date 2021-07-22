4d9a8139daa11a869fb422497d681adecbe9eaed
Francesco De Giorgi <deggio@exact-lab.it>
2021-07-22 12:15:39 +0200

    Update run_ior.sh
    Update mdtest

--- run_ior.sh
+++ run_ior.sh
@@ -101,11 +101,12 @@ done
 # every file/dir will be uniq
 # write 200k for each file, and read it (-w -e)
 # work in -d as root folder
+HOW_MANY_FILES=250000
 for process in 8
 do
-	mpirun -np $process -npernode 2 --hostfile $HOSTFILE -oversubscribe --allow-run-as-root --mca btl self,tcp -display-map -display-allocation --bind-to core:overload-allowed $MDTEST -n 250000 -i 3 -u -d $WORK_DIR/mdtest > $WORK_DIR/mdtest.thread-${process}.out 2> $WORK_DIR/mdtest.thread-${process}_empty_files.err
-	mpirun -np $process -npernode 2 --hostfile $HOSTFILE -oversubscribe --allow-run-as-root --mca btl self,tcp -display-map -display-allocation --bind-to core:overload-allowed $MDTEST -n 250000 -i 3 -u -w 4k -e 4k -d $WORK_DIR/mdtest > $WORK_DIR/mdtest.thread-${process}.out 2> $WORK_DIR/mdtest.thread-${process}_4k-files.err	
-	mpirun -np $process -npernode 2 --hostfile $HOSTFILE -oversubscribe --allow-run-as-root --mca btl self,tcp -display-map -display-allocation --bind-to core:overload-allowed $MDTEST -n 250000 -i 3 -u -w 32k -e 32k -d $WORK_DIR/mdtest > $WORK_DIR/mdtest.thread-${process}.out 2> $WORK_DIR/mdtest.thread-${process}_32k-files.err		
+	mpirun -np $process -npernode 2 --hostfile $HOSTFILE -oversubscribe --allow-run-as-root --mca btl self,tcp -display-map -display-allocation --bind-to core:overload-allowed $MDTEST -n $HOW_MANY_FILES -i 3 -u -d $WORK_DIR/mdtest > $WORK_DIR/mdtest.thread-${process}.out 2> $WORK_DIR/mdtest.thread-${process}_empty_files.err
+	mpirun -np $process -npernode 2 --hostfile $HOSTFILE -oversubscribe --allow-run-as-root --mca btl self,tcp -display-map -display-allocation --bind-to core:overload-allowed $MDTEST -n $HOW_MANY_FILES -i 3 -u -w 4k -e 4k -d $WORK_DIR/mdtest > $WORK_DIR/mdtest.thread-${process}.out 2> $WORK_DIR/mdtest.thread-${process}_4k-files.err	
+	mpirun -np $process -npernode 2 --hostfile $HOSTFILE -oversubscribe --allow-run-as-root --mca btl self,tcp -display-map -display-allocation --bind-to core:overload-allowed $MDTEST -n $HOW_MANY_FILES -i 3 -u -w 32k -e 32k -d $WORK_DIR/mdtest > $WORK_DIR/mdtest.thread-${process}.out 2> $WORK_DIR/mdtest.thread-${process}_32k-files.err		
 done
 
 cd $BASE_DIR
